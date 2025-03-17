78a2c549e4658635d3963ec97e0b1f7f8c06879a
Gustavo Campos <gustavobcampos7@gmail.com>
2025-03-17 15:12:20 -0300

    oth: create table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,10 +4,10 @@
 import { sql } from "drizzle-orm";
 import {
   index,
-  integer,
   pgTableCreator,
   timestamp,
   varchar,
+  boolean,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -16,21 +16,27 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `finance-assistant_${name}`);
+export const createTable = pgTableCreator(
+  (name) => `finance-assistant_${name}`,
+);
 
-export const posts = createTable(
-  "post",
+export const PAID = "PAID";
+export const PENDING = "PENDING";
+
+export const documents = createTable(
+  "documents",
   {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
+    id: varchar("id", { length: 36 }).primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    paid: boolean("paid").default(false).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
